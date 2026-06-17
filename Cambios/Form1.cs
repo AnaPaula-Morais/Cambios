974e734b5dd14179746bc7692e50f60dc6a5ed67
Ana Paula <ap.almeidapm@gmail.com>
2026-06-17 09:51:57 +0100

    Ligacao a API Rates, concluido videos 1, 2 e 3.

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,3 +1,6 @@
+using Cambios.Modelos;
+using Newtonsoft.Json;
+
 namespace Cambios
 {
     public partial class Form1 : Form
@@ -5,8 +8,40 @@ namespace Cambios
         public Form1()
         {
             InitializeComponent();
+            LoadRates();
 
+        }
 
+        private async void LoadRates()
+        {
+            //bool load;
+
+            progressBar1.Value = 0;
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://rates.somee.com");
+
+            var response = await client.GetAsync("/api/Rates");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase);
+                return;
+            }
+
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+
+            comboBoxOrigem.DataSource = rates;
+            comboBoxOrigem.DisplayMember = "Name";
+
+            comboBoxDestino.BindingContext = new BindingContext();
+
+            comboBoxDestino.DataSource = rates;
+            comboBoxDestino.DisplayMember = "Name";
+
+            progressBar1.Value = 100;
         }
     }
 }
